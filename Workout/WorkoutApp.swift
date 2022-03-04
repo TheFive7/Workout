dc75329fd59468a016d167665aa100d0bfe5c5ba
TheFive7 <hennequin.maxime2412@gmail.com>
2022-03-04 08:24:58 +0100

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by hennequin maxime on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
